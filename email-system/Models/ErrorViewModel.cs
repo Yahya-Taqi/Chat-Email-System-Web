d2fcb5e84d5fe930e714927eeb0cd005633f10b2
Muhammad Yahya Taqi <m.yahya.taqi@gmail.com>
2025-02-08 19:14:58 +0500

    Branch Cleanup

--- email-system/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MufaApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
